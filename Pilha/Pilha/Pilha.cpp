34495b7261e3c55095582b601e23ae09b595ae89
Claudio Nunes <claudio@metasistemas.com>
2022-10-04 07:30:05 -0300

    Criacao do projeto base

---
+++ Pilha/Pilha/Pilha.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+// definicao de tipo
+struct NO {
+	int valor;
+	NO* prox;
+};
+
+NO* topo = NULL;
+
+// headers
+void menu();
+void inicializar();
+void pop();
+void push();
+//--------------------------
+
+
+int main()
+{
+	menu();
+}
+
+void menu()
+{
+	int op = 0;
+	while (op != 4) {
+		system("cls"); // somente no windows
+		cout << "Menu Pilha";
+		cout << endl << endl;
+		cout << "1 - Inicializar Pilha \n";
+		cout << "2 - Inserir elemento (Push) \n";
+		cout << "3 - Remover elementos (Pop) \n";
+		cout << "4 - Sair \n";
+
+
+		cout << "Opcao: ";
+		cin >> op;
+
+		switch (op)
+		{
+		case 1: inicializar();
+			break;
+		case 2:push();
+			break;
+		case 3: pop();
+			break;
+		case 4:
+			return;
+		default:
+			break;
+		}
+
+		system("pause"); // somente no windows
+	}
+}
+
+void inicializar()
+{
+
+	// se a lista já possuir elementos
+	// libera a memoria ocupada
+	NO* aux = topo;
+	while (aux != NULL) {
+		NO* paraExcluir = aux;
+		aux = aux->prox;
+		free(paraExcluir);
+	}
+
+	topo = NULL;
+	cout << "Pilha inicializada \n";
+
+}
+
+
+void push()
+{
+	// aloca memoria dinamicamente para o novo elemento
+	NO* novo = (NO*)malloc(sizeof(NO));
+	if (novo == NULL)
+	{
+		return;
+	}
+
+	cout << "Digite o elemento: ";
+	cin >> novo->valor;
+	novo->prox = NULL;
+
+
+}
+
+void pop()
+{
+
+	
+
+}
+
